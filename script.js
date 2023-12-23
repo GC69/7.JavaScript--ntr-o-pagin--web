d2b580060b317e49da3d2a821f87b3daa7b1567e
Crigan Grigore <crigangrigore@gmail.com>
2023-12-24 01:18:42 +0200

    filterByProperty

--- script.js
+++ script.js
@@ -1,86 +1,4 @@
 /*
-// functie ce verifica disponibilitatea stocului
-function checkStock(product) {
-  return product.inStock > 0 ? "In stock" : "Out of stock";
-}
-
-// Array de produse
-const products = [
-  {
-    name: "Numele 1",
-
-    category: "Categoria 1",
-    price: 299.99,
-    originCountry: {
-      name: "Rep.Moldova",
-      code: "MD",
-    },
-    inStock: 6,
-  },
-  {
-    name: "Numele 2",
-    category: "Categoria 2",
-    price: 639.99,
-    originCountry: {
-      name: "România",
-      code: "RO",
-    },
-    inStock: 12,
-  },
-  {
-    name: "Numele 3",
-    category: "Categoria 4",
-    price: 969.99,
-    originCountry: {
-      name: "Ukraine",
-      code: "UKR",
-    },
-    inStock: 3,
-  },
-  {
-    name: "Numele 4",
-    category: "Categoria 3",
-    price: 2369.99,
-    originCountry: {
-      name: "Moldova",
-      code: "MD",
-    },
-    inStock: 0,
-  },
-  {
-    name: "Numele 5",
-    category: "Categoria 1",
-    price: 599.99,
-    originCountry: {
-      name: "România",
-      code: "RO",
-    },
-    inStock: 1,
-  },
-];
-
-// Actualizarea metodei "checkStock" pentru a returna rezultatul
-products.forEach(function (product) {
-  product.availability = checkStock(product);
-});
-
-// Afisam lista de produse.
-function showAllProducts() {
-  products.forEach(function (product) {
-    console.log(product);
-  });
-}
-
-// Posibilitatea de a suplini stocul produselor.
-function addNewProduct(product) {
-  const index = products.findIndex((item) => item.name === product.name);
-  if (index === -1) {
-    products.push(product); // Adauga noul produs in lista
-  }
-  {
-    products[index].inStock = products[index].inStock + product.inStock;
-  }
-}
 
 // Posibilitatea de a lua produse din depozit.
 function removeProduct(name) {
@@ -114,30 +32,6 @@ function filteredByCountry(countryFilter) {
   });
 }
 
-// Să se găsească produsul cel mai scump.
-function findExpensiveProduct() {
-  let expensiveProduct = products[0];
-
-  products.forEach(function (product) {
-    if (product.price > expensiveProduct.price) {
-      expensiveProduct = product;
-    }
-  });
-  return expensiveProduct;
-}
-
-// Să se găsească produsul cel mai ieftin.
-function findCheapestProduct() {
-  let cheapestProduct = products[0];
-
-  products.forEach(function (product) {
-    if (product.price < cheapestProduct.price) {
-      cheapestProduct = product;
-    }
-  });
-  return cheapestProduct;
-}
-
 // Să se găsească produsele cu prețul între o limită indicată
 function findRangeProduct(minValue, maxValue) {
   const foundProducts = [];
@@ -150,18 +44,6 @@ function findRangeProduct(minValue, maxValue) {
 
   return foundProducts;
 }
-
-// addNewProduct({
-//   name: "Numele 5",
-//   category: "Categoria 1",
-//   price: 599.99,
-//   originCountry: {
-//     name: "România",
-//     code: "RO",
-//   },
-//   inStock: 11,
-// });
-
 */
 
 const products = [
@@ -169,7 +51,7 @@ const products = [
     name: "Numele 1",
     description: "Description 1",
     category: "Categoria 1",
-    price: 299.99,
+    price: 300,
     currency: "MDL",
     originCountry: {
       name: "Rep.Moldova",
@@ -227,66 +109,66 @@ const products = [
   },
 ];
 
-function displayProducts() {
-  const section = document.getElementById("productSection");
+function displayProductsDetails(container, product) {
+  const title = document.createElement("h3");
+  const description = document.createElement("p");
+  const category = document.createElement("p");
+  const currency = document.createElement("p");
+  const price = document.createElement("p");
+  const nameCountry = document.createElement("p");
+  const codeCountry = document.createElement("p");
+  const stockAmount = document.createElement("p");
+  const buy = document.createElement("button");
+
+  title.innerHTML = product.name;
+  description.innerHTML = product.description;
+  category.innerHTML = product.category;
+  price.innerHTML = product.price + " " + product.currency;
+  nameCountry.innerHTML =
+    product.originCountry.name + ": " + product.originCountry.code;
+  stockAmount.innerHTML = product.inStock;
+  buy.innerHTML = "Buy";
+
+  container.appendChild(title);
+  container.appendChild(description);
+  container.appendChild(category);
+  container.appendChild(nameCountry);
+  container.appendChild(price);
+  container.appendChild(stockAmount);
+  container.appendChild(buy);
+}
+
+function displayProducts(container, products) {
+  const section = document.querySelector("#productSection");
   section.innerHTML = "";
 
+  console.log("Container:", container, "Products:", products);
   products.forEach((product) => {
     const card = document.createElement("div");
-    const title = document.createElement("h3");
-    const description = document.createElement("p");
-    const category = document.createElement("p");
-    const currency = document.createElement("p");
-    const price = document.createElement("p");
-    const nameCountry = document.createElement("p");
-    const codeCountry = document.createElement("p");
-    const stockAmount = document.createElement("p");
-    const buy = document.createElement("button");
 
-    title.innerHTML = product.name;
-    description.innerHTML = product.description;
-    category.innerHTML = product.category;
-    price.innerHTML = product.price + " " + product.currency;
-    nameCountry.innerHTML =
-      product.originCountry.name + ": " + product.originCountry.code;
-    stockAmount.innerHTML = product.inStock;
-    buy.innerHTML = "Buy";
+    displayProductsDetails(card, product);
 
-    card.appendChild(title);
-    card.appendChild(description);
-    card.appendChild(category);
-    card.appendChild(nameCountry);
-    card.appendChild(price);
-    card.appendChild(stockAmount);
-    card.appendChild(buy);
+    container.appendChild(card);
+
     section.appendChild(card);
   });
 }
 
-function addNewProduct(product) {
-  const index = products.findIndex((item) => item.name === product.name);
-
-  console.log("Index:", index);
-  if (index === -1) {
-    console.log("Adding new product:", product);
-    products.push(product); //add new product in the list
-  } else {
-    products[index].inStock += product.inStock;
-  }
-  return products[index] || products;
-}
-
-function displayNewProduct() {
+function createProductForm() {
   const form = document.createElement("form");
 
   // create input elements for the new product
   const productNameInput = document.createElement("input");
   productNameInput.type = "text";
-  productNameInput.placeholder = "New Product name";
+  productNameInput.placeholder = "Product name";
+
+  const productDescriptionInput = document.createElement("input");
+  productDescriptionInput.type = "text";
+  productDescriptionInput.placeholder = "Product Description";
 
   const productCategoryInput = document.createElement("input");
   productCategoryInput.type = "text";
-  productCategoryInput.placeholder = "New Product category";
+  productCategoryInput.placeholder = "Product category";
 
   const productPriceInput = document.createElement("input");
   productPriceInput.type = "number";
@@ -315,6 +197,7 @@ function displayNewProduct() {
     option.text = code;
     codeCountrySelect.appendChild(option);
   });
+
   // create the select element for the currencies
   const currencySelect = document.createElement("select");
   const currencyOptions = ["MDL", "RON", "EURO", "USD"];
@@ -331,9 +214,12 @@ function displayNewProduct() {
   submitButton.innerHTML = "Add Product";
 
   // add click event on submit button
-  submitButton.addEventListener("click", function () {
-    const newProduct = addNewProduct({
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const newProduct = {
       name: productNameInput.value,
+      description: productDescriptionInput.value,
       category: productCategoryInput.value,
       price: parseFloat(productPriceInput.value),
       currency: currencySelect.value,
@@ -342,103 +228,75 @@ function displayNewProduct() {
         code: codeCountrySelect.value,
       },
       inStock: parseInt(stockAmountInput.value),
-    });
+    };
 
-    form.remove();
+    addNewProduct(newProduct);
 
     // create card for the new product
     const newProductCard = document.createElement("div");
-    const title = document.createElement("h3");
-    title.innerHTML = newProduct.name;
-    const category = document.createElement("p");
-    category.innerHTML = newProduct.category;
-    const currency = document.createElement("p");
-    const price = document.createElement("p");
-    price.innerHTML = newProduct.price + " " + newProduct.currency;
-    const nameCountry = document.createElement("p");
-    const codeCountry = document.createElement("p");
-    nameCountry.innerHTML =
-      newProduct.originCountry.name + ": " + newProduct.originCountry.code;
-    const stockAmount = document.createElement("p");
-    stockAmount.innerHTML = newProduct.inStock;
-    const buy = document.createElement("button");
-    buy.innerHTML = "Buy";
+    newProductCard.className = "New product";
 
-    newProductCard.appendChild(title);
-    newProductCard.appendChild(category);
-    newProductCard.appendChild(nameCountry);
-    newProductCard.appendChild(price);
-    newProductCard.appendChild(stockAmount);
-    newProductCard.appendChild(buy);
+    displayProductsDetails(newProductCard, newProduct);
 
     // append the new product card to the section
     const section = document.querySelector("#productSection");
-    section.appendChild(newProductCard);
+    section.prepend(newProductCard);
+
+    // Show updated list of products
+    displayProducts(section, products);
+
+    console.log(form);
+
+    // reset the form after adding new product
+    form.reset();
   });
 
   // add elements to the form
   form.appendChild(productNameInput);
+  form.appendChild(productDescriptionInput);
   form.appendChild(productCategoryInput);
   form.appendChild(productPriceInput);
+  form.appendChild(stockAmountInput);
   form.appendChild(countrySelect);
   form.appendChild(codeCountrySelect);
   form.appendChild(currencySelect);
-  form.appendChild(stockAmountInput);
   form.appendChild(submitButton);
+
+  return form;
+}
+
+function addNewProduct(product) {
+  const index = products.findIndex((item) => {
+    return (
+      item.name === product.name &&
+      item.category === product.category &&
+      item.originCountry.code === product.originCountry.code &&
+      item.price === product.price &&
+      item.currency === product.currency
+    );
+  });
+
+  console.log("Index:", index);
+  if (index === -1) {
+    console.log("Adding new product:", product);
+    products.push(product); //add new product in the list
+  } else {
+    console.log("Product already exists.Updating quantity");
+    products[index].inStock += product.inStock;
+  }
+
+  console.log(products);
+  return products;
+}
+
+function displayNewProduct() {
+  const form = createProductForm();
 
   // add form to the section
   const section = document.querySelector("#productSection");
-  section.appendChild(form);
-}
 
-function findExpensiveProduct() {
-  let expensiveProduct = products[0];
-
-  products.forEach(function (product) {
-    if (product.price > expensiveProduct.price) {
-      expensiveProduct = product;
-    }
-  });
-  return expensiveProduct;
-}
-
-function displayExpensiveProduct() {
-  let expensiveProduct = findExpensiveProduct();
-
-  const section = document.querySelector("#productSection");
-  const expensiveProductInfo = document.createElement("div");
-  expensiveProductInfo.className = "expensiveProduct";
-
-  const info = document.createElement("h4");
-  info.innerHTML = "Expensive Product is: ";
-
-  const title = document.createElement("h3");
-  title.innerHTML = expensiveProduct.name;
-  const category = document.createElement("p");
-  category.innerHTML = expensiveProduct.category;
-  const currency = document.createElement("p");
-  const price = document.createElement("p");
-  price.innerHTML = expensiveProduct.price + " " + expensiveProduct.currency;
-  const nameCountry = document.createElement("p");
-  const codeCountry = document.createElement("p");
-  nameCountry.innerHTML =
-    expensiveProduct.originCountry.name +
-    ": " +
-    expensiveProduct.originCountry.code;
-  const stockAmount = document.createElement("p");
-  stockAmount.innerHTML = expensiveProduct.inStock;
-  const buy = document.createElement("button");
-  buy.innerHTML = "Buy";
-
-  expensiveProductInfo.appendChild(info);
-  expensiveProductInfo.appendChild(title);
-  expensiveProductInfo.appendChild(category);
-  expensiveProductInfo.appendChild(nameCountry);
-  expensiveProductInfo.appendChild(price);
-  expensiveProductInfo.appendChild(stockAmount);
-  expensiveProductInfo.appendChild(buy);
-
-  section.appendChild(expensiveProductInfo);
+  //append the form to the section
+  section.prepend(form);
 }
 
 function findCheapestProduct() {
@@ -453,48 +311,101 @@ function findCheapestProduct() {
 }
 
 function displayCheapestProduct() {
-  let cheapestProduct = findCheapestProduct();
-
   const section = document.querySelector("#productSection");
-  const cheapestProductInfo = document.createElement("div");
-  cheapestProductInfo.className = "cheapestProduct";
+  section.innerHTML = "";
 
   const info = document.createElement("h4");
+  const cheapestProductInfo = document.createElement("div");
+  const cheapestProduct = findCheapestProduct();
+
   info.innerHTML = "Cheapest Product is: ";
-  const title = document.createElement("h3");
-  title.innerHTML = cheapestProduct.name;
-  const category = document.createElement("p");
-  category.innerHTML = cheapestProduct.category;
-  const currency = document.createElement("p");
-  const price = document.createElement("p");
-  price.innerHTML = cheapestProduct.price + " " + cheapestProduct.currency;
-  const nameCountry = document.createElement("p");
-  const codeCountry = document.createElement("p");
-  nameCountry.innerHTML =
-    cheapestProduct.originCountry.name +
-    ": " +
-    cheapestProduct.originCountry.code;
-  const stockAmount = document.createElement("p");
-  stockAmount.innerHTML = cheapestProduct.inStock;
-  const buy = document.createElement("button");
-  buy.innerHTML = "Buy";
+  cheapestProductInfo.className = "cheapestProduct";
 
-  cheapestProductInfo.appendChild(info);
-  cheapestProductInfo.appendChild(title);
-  cheapestProductInfo.appendChild(category);
-  cheapestProductInfo.appendChild(nameCountry);
-  cheapestProductInfo.appendChild(price);
-  cheapestProductInfo.appendChild(stockAmount);
-  cheapestProductInfo.appendChild(buy);
+  displayProducts(cheapestProductInfo, [cheapestProduct]);
 
-  // cheapestProductInfo.appendChild(divContainer);
-
+  section.prepend(info);
   section.appendChild(cheapestProductInfo);
+}
+
+function findExpensiveProduct() {
+  let expensiveProduct = products[0];
+
+  products.forEach(function (product) {
+    if (product.price > expensiveProduct.price) {
+      expensiveProduct = product;
+    }
+  });
+  return expensiveProduct;
+}
+
+function displayExpensiveProduct() {
+  const section = document.querySelector("#productSection");
+  section.innerHTML = "";
+
+  const expensiveProductInfo = document.createElement("div");
+  const info = document.createElement("h4");
+  const expensiveProduct = findExpensiveProduct();
+
+  info.innerHTML = "Expensive Product is: ";
+  expensiveProductInfo.className = "expensiveProduct";
+
+  displayProducts(expensiveProductInfo, [expensiveProduct]);
+
+  section.prepend(info);
+  section.appendChild(expensiveProductInfo);
+}
+
+function filterByProperty(property, value) {
+  return products.filter(function (product) {
+    return product[property] === value;
+  });
+}
+
+function showProductsByProperty() {
+  const section = document.querySelector("#productSection");
+  section.innerHTML = "";
+
+  const filterSelect = document.createElement("select");
+
+  // add options for select
+  const propertyOptions = ["name", "category", "originCountry.name"];
+  propertyOptions.forEach((property) => {
+    const option = document.createElement("option");
+    option.value = property;
+    option.text = property;
+    filterSelect.appendChild(option);
+  });
+
+  // add select to DOM
+  section.appendChild(filterSelect);
+
+  // add input field for property value
+  const selectedValue = document.createElement("input");
+  selectedValue.type = "text";
+  selectedValue.placeholder = "Enter a value for selected property";
+  section.appendChild(selectedValue);
+
+  // add button for trigger filter
+  const filterButton = document.createElement("button");
+  filterButton.innerHTML = "Filter";
+  filterButton.addEventListener("click", function () {
+    const selectedProperty = filterSelect.value;
+    const selectedPropertyValue = selectedValue.value;
+
+    // filter and show results
+    const filterdProducts = filterByProperty(
+      selectedProperty,
+      selectedPropertyValue
+    );
+
+    displayProducts(document.getElementById("productSection"), filterdProducts);
+  });
+  section.appendChild(filterButton);
 }
 
 function resetFilters() {
   const section = document.querySelector("#productSection");
 
   section.innerHTML = "";
-  displayProducts();
+  // displayProducts();
 }
